323cfe59949aed8b7184e34ec8bdaf6f970076b3
Johan Mathe <johmathe@nonutc.fr>
2015-05-07 08:24:03 -0700

    Fixed travis scripts.

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,4 +1,4 @@
 set -e
 apt-get update -y
-apt-get install -y libavdevice-dev libavformat-dev libgd2-noxpm libgd2-noxpm-dev
+apt-get install -y libswscale2 libswscale-dev libavdevice-dev libavformat-dev libgd2-noxpm libgd2-noxpm-dev
 apt-get install -y libwxgtk2.8-dev
