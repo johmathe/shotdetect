5eb273aba8b522802c462fdddfa861a4350c755a
johmathe <johmathe@nonutc.fr>
2015-05-06 20:21:33 -0700

    Fixed travis script.

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,4 +1,4 @@
 set -e
 apt-get update -y
 apt-get install -y libavdevice-dev libavformat-dev libgd2-noxpm libgd2-noxpm-dev
-apt-get install -y libwxgtk2.6-dev wx2.6-doc libwxgtk2.6-headers
+apt-get install -y libwxgtk2.8-dev
