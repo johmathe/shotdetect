af199935b959e68a7469e36fa9c0176ae24692e7
anand <anand.jeyahar@gmail.com>
2016-06-05 11:41:05 +0530

    more cleanups..

--- shotdetect/src/version.h
+++
@@ -1,8 +0,0 @@
-#ifndef __VERSION_H__
-#define __VERSION_H__
-
-extern const char g_GIT_SHA1[];
-extern const char g_GIT_SHA1_SHORT[];
-extern const char g_APP_VERSION[];
-
-#endif /* !__VERSION_H__ */
